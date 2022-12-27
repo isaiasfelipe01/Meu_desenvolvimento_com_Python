869a70aa2ffbdb54a0095a3eec4e82430e164067
Isaías Felipe Silva de Sousa <isaiasfelipe232@gmail.com>
2022-12-27 17:44:08 -0300

    add exercícios

---
+++ exercicios/ex-011.py
@@ -0,0 +1,4 @@
+largura = float(input('Largura da parede: '))
+altura = float(input('Altura da parede: '))
+print(f'Sua parede tem as dimensões de {largura}x{altura} e sua área é de {largura*altura}m^2.')
+print(f'Para pintar essa parede, você preciasará de {(largura*altura)/2}l de tinta.')
