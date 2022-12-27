addafe2324a7c9526b9c7795404d14ea6f9b3eb4
Isaías Felipe Silva de Sousa <isaiasfelipe232@gmail.com>
2022-12-27 17:54:25 -0300

    add exercícios

---
+++ exercicios/ex-013.py
@@ -0,0 +1,2 @@
+salário = float(input('Qual é o salário do funiconário(a)? R$'))
+print(f'Um funcionário que ganhava R${salário:.2f}, com aumento de 15%, passa a receber R${salário+((salário*15)/100):.2f}.')
