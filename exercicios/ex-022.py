c2982eabf49647518e709a6d75d4dac8952757a6
Isaías Felipe Silva de Sousa <isaiasfelipe232@gmail.com>
2022-12-30 21:13:06 -0300

    add exercícios

---
+++ exercicios/ex-022.py
@@ -0,0 +1,5 @@
+nome = str(input('Seu nome completo: ')).strip()
+print(f'Seu nome em maíusculas é: {nome.upper()}.')
+print(f'Seu nome em minúsculas é: {nome.lower()}.')
+print(f'Seu nome tem {len(nome) - nome.count(" ")}.')
+print(f'Seu primeiro nome tem {nome.find(" ")} letras.')
