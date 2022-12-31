a69276803b6f8b196d2566b3fc10f260696573b7
Isaías Felipe Silva de Sousa <isaiasfelipe232@gmail.com>
2022-12-30 21:27:07 -0300

    add exercícios

---
+++ exercicios/ex-023.py
@@ -0,0 +1,11 @@
+num = int(input('Informe um número: '))
+n = str(num)
+print(f'Com a análise do número {num} temos:')
+
+
+print(
+    f' Unidade:  {n[3]}\n',
+    f'Dezena:   {n[2]}\n',
+    f'Centena:  {n[1]}\n',
+    f'Milhar:   {n[0]}\n',
+)
