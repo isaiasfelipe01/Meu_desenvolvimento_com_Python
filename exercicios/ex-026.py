940028dad1f46aa725df38706b7d59a1531eefab
Isaías Felipe Silva de Sousa <isaiasfelipe232@gmail.com>
2023-01-02 11:47:14 -0300

    add exercícios

---
+++ exercicios/ex-026.py
@@ -0,0 +1,5 @@
+frase = str(input('Digite uma frase: ')).strip().upper()
+
+print('A letra A aparerce {} vezes na frase.'.format(frase.count('A')))
+print('A primeira letra A apareceu na posição {}.'.format(frase.find('A')))
+print('A última letra A apareceu na posição {}'.format(frase.rfind('A')))
